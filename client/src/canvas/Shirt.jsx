dadfe9cc779dce831a9b24e86d63059f2594750b
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-07-25 17:17:36 -0600

    Fixes T-Shirt color issue

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
@@ -7,25 +7,40 @@ import { Decal, useGLTF, useTexture } from '@react-three/drei';
 import state from '../store';
 
 const Shirt = () => {
-  const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+    const snap = useSnapshot(state);
+    const { nodes, materials } = useGLTF('/shirt_baked.glb');
 
-  const logoTexture = useTexture(snap.logoDecal);
-  const fullTexture = useTexture(snap.fullDecal);
+// I added a reference "meshRef" to the mesh component that represents the t-shirt material
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+    const meshRef = useRef();
 
-  const stateString = JSON.stringify(snap);
+    const logoTexture = useTexture(snap.logoDecal);
+    const fullTexture = useTexture(snap.fullDecal);
 
-  return (
-    <group key={stateString}>
-      <mesh
-        castShadow
-        geometry={nodes.T_Shirt_male.geometry}
-        material={materials.lambert1}
-        material-roughness={1}
-        dispose={null}
-      >
+    useFrame((state, delta) => {
+        easing.dampC(materials.lambert1.color, snap.color, 0.25, delta);
+
+        // update the color to the material
+        meshRef.current.material.color = materials.lambert1.color;
+        meshRef.current.material.needsUpdate = true;
+
+        // Set aoMapIntensity to 0
+        materials.lambert1.aoMapIntensity=0;
+
+    });
+
+    const stateString = JSON.stringify(snap);
+
+    return (
+        <group>
+            <mesh
+                ref={meshRef}
+                castShadow
+                geometry={nodes.T_Shirt_male.geometry}
+                material={materials.lambert1}
+                material-roughness={1}
+                dispose={null}
+            >
         {snap.isFullTexture && (
           <Decal 
             position={[0, 0, 0]}
